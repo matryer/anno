bf83c226bfad61232891babc2007e4231decbe98
Mat Ryer <thatmatryer@gmail.com>
2015-05-31 15:58:11 +0100

    added expander example to Readme

--- README.md
+++ README.md
@@ -21,3 +21,27 @@ Found a url at [5:29]: "http://www.websites.com/"
 Found a mention at [48:57]: "@mentions"
 Found a hashtag at [34:43]: "#hashtags"
 ```
+
+You can expand the notes using `anno.Expander`:
+
+```
+expander := anno.Expander{
+	"url": func(b string) string {
+		return fmt.Sprintf(`<a href="%[1]s">%[1]s</a>`, b)
+	},
+	"mention": func(b string) string {
+		return fmt.Sprintf(`<a href="https://downlist.io/%[1]s">%[1]s</a>`, b)
+	},
+}
+src := "This is a #long string written by @mat containing links to https://downlist.io/."
+notes, err := anno.FindManyString(src, anno.URLs, anno.Mentions, anno.Hashtags)
+is.NoErr(err)
+
+log.Println(expander.Expand(src, notes))
+```
+
+Will output:
+
+```
+This is a #long string written by <a href="https://downlist.io/@mat">@mat</a> containing links to <a href="https://downlist.io/">https://downlist.io/</a>.
+```
--- expander_test.go
+++ expander_test.go
@@ -9,11 +9,7 @@ import (
 )
 
 func TestExpander(t *testing.T) {
-
 	is := is.New(t)
-	src := "This is a #long string written by @mat containing links to https://downlist.io/."
-	notes, err := anno.FindManyString(src, anno.URLs, anno.Mentions, anno.Hashtags)
-	is.NoErr(err)
 
 	expander := anno.Expander{
 		"url": func(b string) string {
@@ -23,6 +19,9 @@ func TestExpander(t *testing.T) {
 			return fmt.Sprintf(`<a href="https://downlist.io/%[1]s">%[1]s</a>`, b)
 		},
 	}
+	src := "This is a #long string written by @mat containing links to https://downlist.io/."
+	notes, err := anno.FindManyString(src, anno.URLs, anno.Mentions, anno.Hashtags)
+	is.NoErr(err)
 
 	out := expander.Expand(src, notes)
 	is.Equal(out, `This is a #long string written by <a href="https://downlist.io/@mat">@mat</a> containing links to <a href="https://downlist.io/">https://downlist.io/</a>.`)
