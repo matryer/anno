e5f8f7497c1e8fe8812358503b7209d3f3b6342a
Mat Ryer <thatmatryer@gmail.com>
2015-06-02 22:00:48 +0100

    made naked something.tld match as URL

--- finders.go
+++ finders.go
@@ -8,9 +8,42 @@ func trimPunctuation(s []byte) []byte {
 	return bytes.TrimRight(s, punctuation)
 }
 
+var tlds = [][]byte{
+	[]byte(".com"),
+	[]byte(".net"),
+	[]byte(".org"),
+	[]byte(".edu"),
+	[]byte(".co"),
+	[]byte(".io"),
+	[]byte(".uk"),
+	[]byte(".us"),
+	[]byte(".ca"),
+	[]byte(".de"),
+	[]byte(".jp"),
+	[]byte(".fr"),
+	[]byte(".au"),
+	[]byte(".ru"),
+	[]byte(".ch"),
+	[]byte(".it"),
+	[]byte(".nl"),
+	[]byte(".se"),
+	[]byte(".no"),
+	[]byte(".es"),
+	[]byte(".mil"),
+}
+
 // URL finds web addresses.
 var URLs = FieldFunc("url", func(s []byte) (bool, []byte) {
-	return bytes.HasPrefix(s, []byte("http")) || bytes.HasPrefix(s, []byte("www")), trimPunctuation(s)
+	trimmedS := trimPunctuation(s)
+	if bytes.HasPrefix(s, []byte("http")) || bytes.HasPrefix(s, []byte("www")) {
+		return true, trimmedS
+	}
+	for _, tld := range tlds {
+		if bytes.HasSuffix(s, tld) {
+			return true, trimmedS
+		}
+	}
+	return false, s
 })
 
 // Mentions finds @twitter style mentions.
